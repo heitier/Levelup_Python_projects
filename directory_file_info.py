b7a68a49ddf7bdf2ea5824a9faab148e57b9b3e9
Kanda Mateta <matetah@gmail.com>
2024-01-23 03:43:50 +0000

    version 1

--- directory_file_info.py
+++ directory_file_info.py
@@ -1,3 +1,18 @@
-"""
-Your module description
-"""
+import os
+
+def get_file_information(input_directory="."):
+    information_list = []
+
+    files_in_directory = os.listdir(input_directory)
+
+    for file_name in files_in_directory:
+        file_path = os.path.join(input_directory, file_name)
+        file_size = os.path.getsize(file_path)
+        file_data = {'name': file_name, 'path': file_path, 'size': file_size}
+        information_list.append(file_data)
+
+    return information_list
+
+files_info_result = get_file_information()
+for file_data_result in files_info_result:
+    print(file_data_result)
